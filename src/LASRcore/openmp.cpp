40cfd92082ce3d2e3e2efb7b44519e2cd22ef829
ksemchh <ksemchh@vk.com>
2025-08-06 07:34:22 +0000

    fix: fix for win

--- src/LASRcore/openmp.cpp
+++ src/LASRcore/openmp.cpp
@@ -1,11 +1,5 @@
 #include "openmp.h"
 
-#ifdef HAVE_OMP_GET_THREAD_LIMIT
-    return omp_get_thread_limit();
-#else
-    return omp_get_max_threads();
-#endif
-
 int available_threads()
 {
   int max = omp_get_max_threads();
